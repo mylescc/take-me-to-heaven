39dcd35d406dcdf471d3edf62060262873eb3e62
Erik Nygren <erik.nygren6@gmail.com>
2016-02-02 19:37:05 +0000

    disabling the mad tunes for now

--- heavenController.js
+++ heavenController.js
@@ -5,6 +5,7 @@
     this.views = ['splash', 'geoquery', 'postcode', 'loadingAPI', 'result'];
     this.currentViewIndex = 0;
     this.currentView = this.views[this.currentViewIndex];
+    this.soundEnabled = false;
 
     function init() {
       if ("geolocation" in navigator) {
