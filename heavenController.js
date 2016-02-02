815a57f075338b84de84c6e42a8b6da7663b2f26
Myles Cowper-Coles <myles@farmdrop.co.uk>
2016-02-02 19:42:26 +0000

    Adding the google maps service

--- heavenController.js
+++ heavenController.js
@@ -5,10 +5,12 @@
     this.views = ['splash', 'geoquery', 'postcode', 'loadingAPI', 'result'];
     this.currentViewIndex = 0;
     this.currentView = this.views[this.currentViewIndex];
+    var directionService;
+    
 
     function init() {
       if ("geolocation" in navigator) {
-          console.log('init');
+        console.log('init');
       } else {
         alert('fuck you and your old ass browser, site will not work m8');
         location.href = 'http://bfy.tw/C9E';
@@ -19,14 +21,29 @@
       this.nextView(1);
       navigator.geolocation.getCurrentPosition(function(position) {
         console.log(position.coords.latitude, position.coords.longitude);
-
         this.queryAPI(position.coords.latitude, position.coords.longitude, null);
       }.bind(this));
     };
 
     this.queryAPI = function(lat, lng, postcode) {
-      this.nextView(3);
+      if (directionService === undefined) {
+        directionService = new google.maps.DirectionsService();
+      }
+      var origin = new google.maps.LatLng(lat, lng);
+      var request = {
+        origin: origin,
+        destination: 'Wetherspoons',
+        travelMode: google.map.TravelMode.TRANSIT,
+        provideRouteAlternatives: false,
+        region: GB
+      }
+      directionService.route(request, function(results, status) {
+        console.log(results);
+        console.log(status);
+      });
+
       $timeout(this.apiCallback, 3000);
+      this.nextView(3);
     };
 
     this.apiCallback = function(result) {
